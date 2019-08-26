d0ff97a34424e5de288e74c35a966772421fe506
Maria Yanova <myanova13@gmail.com>
2019-08-26 21:06:14 +0300

    Второе задание

--- script.js
+++ script.js
@@ -1,24 +1,75 @@
-'use strict';
-
 let money = Number.parseInt(prompt("Ваш бюджет на месяц?"));
 let time = Date.parse(prompt("Введите дату в формате YYYY-MM-DD"));
 let countOfQuestions = 2;
 let allExpenceCost = 0;
 let daysInMonth = 30;
 let appData = {
-    moneyData: money,
+    budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
-    income: {},
-    savings: false
+    income: [],
+    savings: false,
+    moneyPerDay: 0
 };
 
-for (var i = 0; i < countOfQuestions; ++i) {
-    let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
-    let expenseCost = Number.parseInt(prompt("Во сколько обойдется?"));
-    appData.expenses[i] = expenseItem + " : " + expenseCost;
-    allExpenceCost += expenseCost;
-}
 
-alert("Бюджет на один день: " + ((appData.moneyData - allExpenceCost) / daysInMonth).toFixed(2));
+for (let i = 0; i < countOfQuestions; ++i) {
+    let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
+    let expenseCost = prompt("Во сколько обойдется?");
+
+    if (typeof (expenseItem) === 'string' && typeof (expenseItem) != null && expenseItem != '' && expenseItem.length < 50 &&
+        typeof (expenseCost) === 'string' && typeof (expenseCost) != null && expenseCost != '') {
+        appData.expenses[expenseItem] = Number.parseInt(expenseCost);
+        allExpenceCost += Number.parseInt(expenseCost);
+    } else {
+        --i;
+    }
+};
+
+/*
+let i = 0;
+while (i < countOfQuestions) {
+    let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
+    let expenseCost = prompt("Во сколько обойдется?");
+
+    if (typeof (expenseItem) === 'string' && typeof (expenseItem) != null && expenseItem != '' && expenseItem.length < 50 &&
+        typeof (expenseCost) === 'string' && typeof (expenseCost) != null && expenseCost != '') {
+        
+        appData.expenses[expenseItem] = Number.parseInt(expenseCost);
+        allExpenceCost += Number.parseInt(expenseCost);
+        ++i;
+    }
+}
+*/
+
+/*
+let i = 0;
+do {
+    let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
+    let expenseCost = prompt("Во сколько обойдется?");
+
+    if (typeof (expenseItem) === 'string' && typeof (expenseItem) != null && expenseItem != '' && expenseItem.length < 50 &&
+        typeof (expenseCost) === 'string' && typeof (expenseCost) != null && expenseCost != '') {
+
+        appData.expenses[expenseItem] = Number.parseInt(expenseCost);
+        allExpenceCost += Number.parseInt(expenseCost);
+        ++i;
+    }
+}
+while (i < countOfQuestions)
+*/
+
+appData.moneyPerDay = ((appData.budget - allExpenceCost) / daysInMonth).toFixed(2);
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
