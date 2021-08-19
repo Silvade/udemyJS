a1fbd2bbaed4c0e4c66ee868e8bc28de18640f27
Silvade <myanova13@gmail.com>
2021-08-19 22:14:29 +0300

    ex15

--- ex12.js
+++ ex12.js
@@ -9,10 +9,27 @@ const personalMovieDB = {
 };
 
 const count = 2;
-for (var i = 0; i < count; ++i) {
+for (let i = 0; i < count; ++i) {
     const lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
+    if (lastFilmName === null || lastFilmName.length < 1 || lastFilmName.length > 50) {
+        --i;
+        continue;
+    }
     const lastFilmRaiting = prompt('На сколько оцените его?', '');
+    if (lastFilmRaiting === null || lastFilmRaiting.length < 1) {
+        --i;
+        continue;
+    }
     personalMovieDB.movies[lastFilmName] = lastFilmRaiting;
-};
-
+}
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 31) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
